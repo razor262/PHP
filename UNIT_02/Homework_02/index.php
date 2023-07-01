2bad8ea4b14d119a8747470b13a4b9bf757ebc18
Seven <34629679+razor262@users.noreply.github.com>
2023-07-01 12:30:26 +0300

    Add files via upload

---
+++ UNIT_02/Homework_02/index.php
@@ -0,0 +1,85 @@
+<?php 
+///////////////////////////////
+echo('Task 2');  
+echo '<br>';  
+echo '<hr>';
+
+$a = 5;  
+var_dump($a); // int(5)
+echo '<br>';  
+
+$b = 3.14;  
+var_dump($b); // float(3.14) 
+echo '<br>';  
+
+$c = -10;  
+var_dump($c); // int(-10)   
+echo '<br>';  
+
+$d = "test";  // string(4) 
+var_dump($d);   
+echo '<br>';  
+
+$e = true;  
+var_dump($e);  // bool(true)
+echo '<br>';  
+
+$f = false;  
+var_dump($f);  //bool(false)
+echo '<br>';  
+echo '<hr>';  
+
+////////////////////////////////////
+echo('Task 4');  
+echo '<br>';  
+echo '<hr>';  
+
+$j = 8.14;  
+var_dump($j);  // float   
+echo '<br>';  
+
+$h = "5Hello";  
+var_dump($h);  // string 
+echo '<br>';  
+
+$k = 15.7;  
+var_dump($k);  // float 
+echo '<br>';  
+
+$l = "test hi";
+var_dump($l);  // string 
+echo '<br>';  
+
+$m = false;  
+var_dump($m);  // boolean false
+echo '<br>';  
+
+$n = true;
+var_dump($n); // boolean true 
+echo '<br>';  
+
+$o = false;  
+var_dump($o); // boolean false 
+echo '<br>';  
+
+$p = true;  
+var_dump($p); // boolean true 
+echo '<br>';  
+echo '<hr>';  
+
+
+////////////////////////////////////
+echo('Task 5');  
+echo '<br>';  
+echo '<hr>'; 
+
+function f1(){
+    $a = 12;
+    $k = 64;  
+    echo $a.$k; 
+}  
+
+f1();  
+
+
+echo '<hr>'; 
---
+++ UNIT_02/Video_02/index.php
@@ -0,0 +1,30 @@
+<?php  
+$a = 5;  
+echo $a;  
+echo '<p>Hello</p>';  
+$a = '<div>Test</div>';
+$a = 8;    
+echo $a;  
+echo '<br>';  
+var_dump($a);  
+// int float string bool
+$c = 10 % $a; // + - * / % 
+$d = 'mystr';  
+
+echo '<br>';  
+echo $d.' hi'; //.
+echo '<br>';
+
+var_dump($a != 822); // ==  
+
+echo '<br>';
+function myFunc(){
+    // тело функции  
+    echo '<hr>';   
+    $a = 111111;   
+    echo 111111*$a;  
+}  
+
+myFunc(); 
+
+
